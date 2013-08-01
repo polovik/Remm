935754e0df065a04650ba4b4dbc9de9260b1a614
dmitry.valento <decobramegra@gmail.com>
2013-08-01 18:41:20 +0300

    Added possibility direct connect via IPv4 UDP socket.

--- clientQt/Settings.qml
+++ clientQt/Settings.qml
@@ -7,30 +7,92 @@ Item {
     signal connectionEstablished()
     Rectangle {
         id: rectangle1
+        x: 0
+        y: 0
         color: "yellowgreen"
+        anchors.rightMargin: 0
+        anchors.bottomMargin: 0
+        anchors.leftMargin: 0
+        anchors.topMargin: 0
         anchors.fill: parent
 
+        ExclusiveGroup { id: groupConnectionType }
+
+        RadioButton {
+            id: radio_button_IPv6
+            anchors.top: parent.top
+            anchors.topMargin: (parent.height - height) / 3
+            anchors.left: parent.left
+            anchors.leftMargin: (parent.width - width - text_IPv6_RPi_URL.width) / 3
+            text: "IPv6(conection over 3G Internet)"
+            exclusiveGroup: groupConnectionType
+            onCheckedChanged: {
+                if (checked)
+                    text_IPv6_RPi_URL.enabled = true
+                else
+                    text_IPv6_RPi_URL.enabled = false
+            }
+        }
+
         TextField {
-            id: textRPiURL
+            id: text_IPv6_RPi_URL
             width: 209
             height: 25
+            anchors.top: radio_button_IPv6.top
+            anchors.left: radio_button_IPv6.right
+            anchors.leftMargin: (parent.width - width - radio_button_IPv6.width) / 3
             text: "remm.broker.freenet6.net"
+            enabled: false
+            visible: true
+//                anchors.top: parent.top
+//                anchors.topMargin: (parent.height - height) / 2
+//                anchors.left: parent.left
+//                anchors.leftMargin: (parent.width - width) / 2
+            placeholderText: ""
+        }
+
+        RadioButton {
+            id: radio_button_IPv4
+            anchors.top: radio_button_IPv6.bottom
+            anchors.topMargin: 50
+            anchors.left: radio_button_IPv6.left
+            text: "IPv4(direct Ethernet connection)"
+            checked: true
+            exclusiveGroup: groupConnectionType
+            onCheckedChanged: {
+                if (checked)
+                    text_IPv4_RPi_IP.enabled = true
+                else
+                    text_IPv4_RPi_IP.enabled = false
+            }
+        }
+
+        TextField {
+            id: text_IPv4_RPi_IP
+            width: 209
+            height: 25
+            anchors.top: radio_button_IPv4.top
+            anchors.left: text_IPv6_RPi_URL.left
+            text: "192.168.1.100"
+            placeholderText: ""
+        }
+
+        Label {
+            id: label_Port
             anchors.top: parent.top
-            anchors.topMargin: (parent.height - height) / 2
-            anchors.left: parent.left
-            anchors.leftMargin: (parent.width - width) / 2
-            placeholderText: "Text Field"
+            anchors.topMargin: (parent.height - height) / 3 * 2
+            anchors.left: buttonConnectRPi.left
+            text: "Port:"
         }
 
         TextField {
             id: textRPiPort
             width: 68
             height: 25
+            anchors.verticalCenter: label_Port.verticalCenter
+            anchors.right: buttonConnectRPi.right
             text: "9512"
-            anchors.top: parent.top
-            anchors.topMargin: (parent.height - height) / 2
-            anchors.left: parent.left
-            anchors.leftMargin: textRPiURL.x + textRPiURL.width + 50
+            readOnly: true
             placeholderText: "Text Field"
         }
 
@@ -38,16 +100,21 @@ Item {
             id: buttonConnectRPi
             width: 117
             height: 32
+            anchors.top: parent.top
+            anchors.topMargin: (parent.height - height) / 6 * 5
+            anchors.left: parent.left
+            anchors.leftMargin: (parent.width - width) / 2
             text: "Connect"
-            anchors.top: textRPiPort.bottom
-            anchors.topMargin: 20
-            anchors.right: textRPiPort.right
             checkable: false
             checked: false
             onClicked: {
                 connectionEstablished()
-                connectRPi.tryDirectConnectToRPi(textRPiURL.text, Number(textRPiPort.text))
+                if (radio_button_IPv6.checked)
+                    connectRPi.tryDirectConnectToRPi(text_IPv6_RPi_URL.text, Number(textRPiPort.text))
+                else
+                    connectRPi.tryDirectConnectToRPi(text_IPv4_RPi_IP.text, Number(textRPiPort.text))
             }
         }
+
     }
 }
--- server/main.c
+++ server/main.c
@@ -35,50 +35,75 @@ typedef struct {
 static server_ctx_s server_ctx;
 static pthread_t command_rx_thread;
 static int exit_thread = 0;
-static struct sockaddr_in6 client_udp_addr;
-static int udp_socket = -1;
+static struct sockaddr_in6 client_udp_ipv6_addr;
+static int udp_ipv6_socket = -1;
+static struct sockaddr_in client_udp_ipv4_addr;
+static int udp_ipv4_socket = -1;
 
 void *command_rx(void *arg)
 {
 	control_packet_s *control_packet;
-	struct sockaddr_in6 udp_addr, remote_udp_addr;
+	struct sockaddr_in6 udp_ipv6_addr, remote_udp_ipv6_addr;
+	struct sockaddr_in udp_ipv4_addr, remote_udp_ipv4_addr;
 	socklen_t addr_len;
 	int bytes_read;
 	unsigned char data[1024];
 	int new_client = 0;
 
-    udp_socket = socket(AF_INET6, SOCK_DGRAM, 0);
-    if (udp_socket < 0) {
-        perror("Creating UDP socket");
+	/*	Create and init IPv6 socket	*/
+    udp_ipv6_socket = socket(AF_INET6, SOCK_DGRAM, 0);
+    if (udp_ipv6_socket < 0) {
+        perror("Creating UDP IPv6 socket");
         exit_thread = 1;	//	Exit from program
         return 0;
     }
-    memset(&udp_addr, 0x00, sizeof(udp_addr));
-    udp_addr.sin6_family = AF_INET6;
-    udp_addr.sin6_port = htons(9512);
-    udp_addr.sin6_addr = in6addr_any;
-    if (bind(udp_socket, (struct sockaddr *)&udp_addr, sizeof(udp_addr)) < 0) {
-        perror("Binding UDP socket");
+    memset(&udp_ipv6_addr, 0x00, sizeof(udp_ipv6_addr));
+    udp_ipv6_addr.sin6_family = AF_INET6;
+    udp_ipv6_addr.sin6_port = htons(9512);
+    udp_ipv6_addr.sin6_addr = in6addr_any;
+    if (bind(udp_ipv6_socket, (struct sockaddr *)&udp_ipv6_addr, sizeof(udp_ipv6_addr)) < 0) {
+        perror("Binding UDP IPv6 socket");
+        exit_thread = 1;	//	Exit from program
+        return 0;
+    }
+
+	/*	Create and init IPv4 socket	*/
+    udp_ipv4_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_ipv4_socket < 0) {
+        perror("Creating UDP IPv4 socket");
+        exit_thread = 1;	//	Exit from program
+        return 0;
+    }
+    memset(&udp_ipv4_addr, 0x00, sizeof(udp_ipv4_addr));
+    udp_ipv4_addr.sin_family = AF_INET;
+    udp_ipv4_addr.sin_port = htons(9512);
+    udp_ipv4_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(udp_ipv4_socket, (struct sockaddr *)&udp_ipv4_addr, sizeof(udp_ipv4_addr)) < 0) {
+        perror("Binding UDP IPv4 socket");
         exit_thread = 1;	//	Exit from program
         return 0;
     }
 
     while (exit_thread == 0) {
 		usleep(1000 * 1000);
-		addr_len = sizeof(remote_udp_addr);
+
+		/*	Check IPv6 pending datagram	*/
+		if (udp_ipv6_socket <= 0)
+			goto ipv4_check;
+		addr_len = sizeof(remote_udp_ipv6_addr);
 		memset(data, 0x00, sizeof(data));
 		errno = 0;
 		//	TODO Add correct working with socket - add mutex and move to separate thread
-		bytes_read = recvfrom(udp_socket, data, sizeof(data),
-							  MSG_DONTWAIT,
-							  (struct sockaddr *)&remote_udp_addr, &addr_len);
+		bytes_read = recvfrom(udp_ipv6_socket, data, sizeof(data), MSG_DONTWAIT,
+							  (struct sockaddr *)&remote_udp_ipv6_addr, &addr_len);
 		if (bytes_read <= 0) {
 			if (errno == EAGAIN) {
 //				printf("INFO  %s() Avoid blocking\n", __FUNCTION__);
+				goto ipv4_check;
 			} else {
-				perror("Receive data from UDP socket");
+				perror("Receive data from UDP IPv6 socket");
+				continue;
 			}
-			continue;
 		}
 
 		set_rgb_led_mode(RGB_GREEN_SINGLE_SHOT);
@@ -92,22 +117,69 @@ void *command_rx(void *arg)
 			continue;
 		}
 
-		if (addr_len != sizeof(remote_udp_addr)) {
+		if (addr_len != sizeof(remote_udp_ipv6_addr)) {
 			printf("ERROR %s() Received command non-IPv6 net\n", __FUNCTION__);
 			continue;
 		}
 
-		new_client = memcmp(&remote_udp_addr.sin6_addr, &client_udp_addr.sin6_addr, sizeof(client_udp_addr.sin6_addr)) != 0;
-		new_client |= memcmp(&remote_udp_addr.sin6_port, &client_udp_addr.sin6_port, sizeof(client_udp_addr.sin6_port)) != 0;
+		new_client = memcmp(&remote_udp_ipv6_addr.sin6_addr, &client_udp_ipv6_addr.sin6_addr, sizeof(client_udp_ipv6_addr.sin6_addr)) != 0;
+		new_client |= memcmp(&remote_udp_ipv6_addr.sin6_port, &client_udp_ipv6_addr.sin6_port, sizeof(client_udp_ipv6_addr.sin6_port)) != 0;
 		if (new_client != 0) {
-			printf("INFO  %s() Connection established\n", __FUNCTION__);
+			printf("INFO  %s() Connection IPv6 established\n", __FUNCTION__);
 			server_ctx.connection_established = 1;
-			memcpy(&client_udp_addr, &remote_udp_addr, sizeof(client_udp_addr));
+			memcpy(&client_udp_ipv6_addr, &remote_udp_ipv6_addr, sizeof(client_udp_ipv6_addr));
+			unsigned int timeout = 1000. / server_ctx.capture_fps;
+			add_timer(timeout, &server_ctx.send_frame_timer);
+			add_timer(STATUS_PACKET_TIMEOUT, &server_ctx.send_status_timer);
+		}
+		goto do_cmd;
+
+ipv4_check:
+		if (udp_ipv4_socket <= 0)
+			goto do_cmd;
+		addr_len = sizeof(remote_udp_ipv4_addr);
+		memset(data, 0x00, sizeof(data));
+		errno = 0;
+		//	TODO Add correct working with socket - add mutex and move to separate thread
+		bytes_read = recvfrom(udp_ipv4_socket, data, sizeof(data), MSG_DONTWAIT,
+							  (struct sockaddr *)&remote_udp_ipv4_addr, &addr_len);
+		if (bytes_read <= 0) {
+			if (errno == EAGAIN) {
+				continue;
+			} else {
+				perror("Receive data from UDP IPv4 socket");
+				continue;
+			}
+		}
+
+		set_rgb_led_mode(RGB_GREEN_SINGLE_SHOT);
+		if (bytes_read != sizeof(control_packet_s)) {
+			printf("ERROR %s() Unexpected packet size %d: %.*s\n", __FUNCTION__, bytes_read, bytes_read, data);
+			continue;
+		}
+		control_packet = (control_packet_s *)data;
+		if (control_packet->magic != MAGIC_COMMAND) {
+			printf("ERROR %s() Unexpected packet type %d: %.*s\n", __FUNCTION__, control_packet->magic, bytes_read, data);
+			continue;
+		}
+
+		if (addr_len != sizeof(remote_udp_ipv4_addr)) {
+			printf("ERROR %s() Received command non-IPv4 net\n", __FUNCTION__);
+			continue;
+		}
+
+		new_client = memcmp(&remote_udp_ipv4_addr.sin_addr.s_addr, &client_udp_ipv4_addr.sin_addr.s_addr, sizeof(client_udp_ipv4_addr.sin_addr.s_addr)) != 0;
+		new_client |= memcmp(&remote_udp_ipv4_addr.sin_port, &client_udp_ipv4_addr.sin_port, sizeof(client_udp_ipv4_addr.sin_port)) != 0;
+		if (new_client != 0) {
+			printf("INFO  %s() Connection IPv4 established\n", __FUNCTION__);
+			server_ctx.connection_established = 1;
+			memcpy(&client_udp_ipv4_addr, &remote_udp_ipv4_addr, sizeof(client_udp_ipv4_addr));
 			unsigned int timeout = 1000. / server_ctx.capture_fps;
 			add_timer(timeout, &server_ctx.send_frame_timer);
 			add_timer(STATUS_PACKET_TIMEOUT, &server_ctx.send_status_timer);
 		}
 
+do_cmd:
 		server_ctx.height = control_packet->height;
 		server_ctx.direction = control_packet->direction;
 		server_ctx.slope = control_packet->slope;
@@ -127,6 +199,31 @@ void *command_rx(void *arg)
     return 0;
 }
 
+static void send_datagram(void *data, size_t length)
+{
+	int sended_bytes;
+
+	errno = 0;
+	//	TODO Add correct working with socket - add mutex and move to separate thread
+	if (udp_ipv6_socket > 0) {
+		sended_bytes = sendto(udp_ipv6_socket, &data, length, 0,
+					   (struct sockaddr *)&client_udp_ipv6_addr, sizeof(client_udp_ipv6_addr));
+	} else if (udp_ipv4_socket > 0) {
+		sended_bytes = sendto(udp_ipv4_socket, &data, length, 0,
+					   (struct sockaddr *)&client_udp_ipv4_addr, sizeof(client_udp_ipv4_addr));
+	} else {
+		printf("ERROR %s() All UDP sockets are invalidated\n", __FUNCTION__);
+	}
+
+	if (sended_bytes < 0) {
+		if (errno == EAGAIN) {
+			printf("INFO  %s() Avoid blocking sending status through UDP socket\n", __FUNCTION__);
+		} else {
+			perror("Sending status through UDP socket");
+		}
+	}
+}
+
 void send_picture(unsigned char *picture, unsigned int length)
 {
 	static unsigned int picture_id = 0;
@@ -134,7 +231,6 @@ void send_picture(unsigned char *picture, unsigned int length)
 	unsigned int fragments_count;
 	unsigned int last_fragment_size;
 	picture_packet_s packet;
-	int sended_bytes;
 
 	printf("INFO  %s() Send picture. Size = %d.\n", __FUNCTION__, length);
 //	printf("%d - %d - %d\n", sizeof(picture_packet_s), sizeof(raw_packet), sizeof(char *));
@@ -150,17 +246,7 @@ void send_picture(unsigned char *picture, unsigned int length)
 		packet.fragment_size = FRAGMENT_SIZE;
 		memcpy(packet.data, &picture[i * FRAGMENT_SIZE], FRAGMENT_SIZE);
 
-		errno = 0;
-		//	TODO Add correct working with socket - add mutex and move to separate thread
-		sended_bytes = sendto(udp_socket, &packet, sizeof(picture_packet_s), 0,
-					   (struct sockaddr *)&client_udp_addr, sizeof(client_udp_addr));
-		if (sended_bytes < 0) {
-			if (errno == EAGAIN) {
-				printf("INFO  %s() Avoid blocking sending status\n", __FUNCTION__);
-			} else {
-				perror("Sending status UDP socket");
-			}
-		}
+		send_datagram(&packet, sizeof(picture_packet_s));
 	}
 
 	last_fragment_size = length - fragments_count * FRAGMENT_SIZE;
@@ -170,17 +256,7 @@ void send_picture(unsigned char *picture, unsigned int length)
 		packet.fragment_size = last_fragment_size;
 		memcpy(packet.data, &picture[i * FRAGMENT_SIZE], last_fragment_size);
 
-		errno = 0;
-		//	TODO Add correct working with socket - add mutex and move to separate thread
-		sended_bytes = sendto(udp_socket, &packet, sizeof(picture_packet_s), 0,
-					   (struct sockaddr *)&client_udp_addr, sizeof(client_udp_addr));
-		if (sended_bytes < 0) {
-			if (errno == EAGAIN) {
-				printf("INFO  %s() Avoid blocking sending status\n", __FUNCTION__);
-			} else {
-				perror("Sending status UDP socket");
-			}
-		}
+		send_datagram(&packet, sizeof(picture_packet_s));
 	}
 
 	printf("INFO  %s() Picture %d has sent.\n", __FUNCTION__, picture_id);
@@ -266,17 +342,8 @@ int main(int argc, char *argv[])
 			}
 			status_packet.slope = server_ctx.slope;
 			status_packet.battery_charge = get_battery_charge();
-			errno = 0;
-			//	TODO Add correct working with socket - add mutex and move to separate thread
-			ret = sendto(udp_socket, &status_packet, sizeof(status_packet_s), 0,
-						 (struct sockaddr *)&client_udp_addr, sizeof(client_udp_addr));
-			if (ret < 0) {
-				if (errno == EAGAIN) {
-					printf("INFO  %s() Avoid blocking sending status\n", __FUNCTION__);
-				} else {
-					perror("Sending status through UDP socket");
-				}
-			}
+
+			send_datagram(&status_packet, sizeof(status_packet_s));
 			add_timer(STATUS_PACKET_TIMEOUT, &server_ctx.send_status_timer);
 		}
 	}
